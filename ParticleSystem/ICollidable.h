379a4dfc3bec624c5437be598a13b5ffba1b7d52
unknown <mooneyot@.itserv.scss.tcd.ie>
2012-01-19 18:45:47 +0000

    did a thing

--- ParticleSystem/ICollidable.h
+++ ParticleSystem/ICollidable.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include "svl/svl.h"
+
+class AABB;
+class Sphere;
+
 class ICollidable
 {
 public:
-	
+	virtual bool PointIntersects(Vec3 point) = 0;
+	virtual bool Intersects(Sphere* other) = 0;	
 };
 
